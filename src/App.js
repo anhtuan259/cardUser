affc201a726c964ad1329ecd1cc12b4b8cc399c2
anhtuan259 <daoanhtuanfake@gmail.com>
2021-03-29 09:34:19 +0700

    up all

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React, { useState } from "react";
-import HelloWorld from "./HelloWorld";
+import React from "react";
+import user from "./images/user.png";
+import "./App.css";
 
-const App = () => {
-  const [tech] = useState("React");
-  return <HelloWorld tech={tech} />;
-};
+class App extends React.Component {
+  state = {
+    name: 'alex bakery',
+    location: 'localhost',
+    description: 'software, speaker',
+    likes: 'cats, wine'
+  }
+  render() {
+    const { name, location, description, likes } = this.state;
+    return (
+      <div className="App">
+        <section className="image">
+          <img src={user} alt="user" />
+        </section>
+        <section className="infor">
+          <p>
+            {" "}
+            <span className="infor_user">I am</span> a {description}
+          </p>
+          <p>
+            {" "}
+            <span className="infor_user">I like</span> {likes}
+          </p>
+          <hr />
+
+          <p>
+            <span className="infor_user">Name:</span>
+            <span className="des_user">{name}</span>
+          </p>
+          <p>
+            <span className="infor_user">Location: </span>
+            <span className="des_user">{location}</span>
+          </p>
+        </section>
+      </div>
+
+    )
+  }
+}
 
 export default App;
